6f83c8ded801ce40bac5f549c3285af7bcabad4b
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2017-06-03 23:24:53 +0300

    Initial commit

---
+++ detect.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * @author: yevgen
+ * @date: 03.06.17
+ */
+require_once __DIR__.'/functions.php';
+
+$dom = new DOMDocument();
+//@$dom->loadHTML(file_get_contents(__DIR__.'/example.html'));
+$html = file_get_contents(__DIR__ . '/nytimes_1.html');
+$tidy = tidy_parse_string($html);
+$tidy->cleanRepair();
+$html = $tidy->value;
+
+@$dom->loadHTML($html);
+
+$nodeList = $dom->getElementsByTagName('*');
+$nodes = iterator_to_array($nodeList);
+$nodelBlacklist = explode(',', 'meta,link,style,script');
+
+
+removeNodes($dom, filterByNodeType([XML_COMMENT_NODE]));
+removeNodes($dom, fFilterByTags($nodelBlacklist));
+//echo $dom->saveHTML(); die();
+//removeNodes($dom->getElementsByTagName('head')->item(0), orFilter(filterByNodeType([XML_COMMENT_NODE]), fFilterByTags($nodelBlacklist)));
+//foreach ($nodes as $el) {
+//    if ($el instanceof DOMElement) {
+//        if (in_array(strtolower($el->tagName), $nodelBlacklist, true)) {
+//            $el->parentNode->removeChild($el);
+//        }
+//    }
+//}
+
+$ignoreList = explode(',', 'button,form');
+
+/** @var DOMElement $el */
+$filter = andFilter(fFilterElement(), fNot(fFilterByTags($ignoreList)));
+foreach (iterateDeepFirst($dom->getElementsByTagName('body')->item(0), $filter) as $el) {
+    if (isContent($el)) {
+        markContent($el);
+        unmarkAsContent($el->childNodes);
+    }
+}
+
+/** @var DOMElement $el */
+//foreach (iterateDeepFirst($dom->getElementsByTagName('body')->item(0), fFilterElement()) as $el) {
+//    echo $el->tagName, ': ', $el->textContent, PHP_EOL;
+//}
+//echo "\n\nHTML:\n";
+//echo $dom->saveHTML(); die();
+//echo $dom->saveHTMLFile(__DIR__.'/result.html');
+
+
+
+$query = new DOMXPath($dom);
+//$nodes = iterator_to_array($query->query('//*[@is-content]'), false);
+
+$blocks = ContentBlock::createFromNodeList($query->query('//*[@is-content]'));
+
+//var_dump($blocks);
+
+$contentBlocks = [];
+$it = new CallbackFilterIterator(iterateThree($blocks), function (array $triplet) use (&$contentBlocks) {
+    if (classifyByDensity($triplet[0], $triplet[1], $triplet[2])) {
+        $contentBlocks[] = $triplet[1];
+    }
+});
+iterator_to_array($it);
+//var_dump($contentBlocks);
+array_walk($contentBlocks, function (ContentBlock $block) {
+   echo $block->getTextContent(), PHP_EOL, PHP_EOL;
+});
+
+//$longTextNodes = new CallbackFilterIterator(new ArrayIterator($nodes), function (DOMNode $node) {
+//    return mb_strlen($node->textContent, 'utf-8') > 25;
+//});
+//array_walk(iterator_to_array($longTextNodes), function (DOMNode $node, $i) {
+//    echo $i, ': ', $node->textContent, PHP_EOL, PHP_EOL;
+//});
+
+
+
+class ContentBlock
+{
+    /**
+     * @var \DOMElement
+     */
+    private $container;
+    /**
+     * @var float
+     */
+    private $linkDensity;
+    /**
+     * @var float
+     */
+    private $textDensity;
+
+    public static function createFromNodeList(DOMNodeList $nodes)
+    {
+        $blocks = [];
+        foreach ($nodes as $node) {
+            $blocks[] = new static($node);
+        }
+
+        return $blocks;
+    }
+
+    /**
+     * ContentBlock constructor.
+     * @param DOMElement $container
+     */
+    public function __construct(DOMElement $container)
+    {
+        $this->container = $container;
+        $this->initDensities();
+    }
+
+    private function initDensities()
+    {
+        $words = mb_split('\s+', $this->container->textContent);
+        $numWords = count($words);
+
+        $numWrappedLines = 0;
+        $lineLimit = 80;
+        $sum = 0;
+        $numWordsCurrentLine = 1;
+        foreach ($words as $word) {
+            $len = mb_strlen($word, 'utf-8');
+            if ($len > $lineLimit) {
+                $numWrappedLines++;
+                $numWordsCurrentLine = 0;
+                $sum = 0;
+                continue;
+            }
+            if (($sum + $len) > $lineLimit) {
+                $numWrappedLines++;
+                $numWordsCurrentLine = 0;
+                $sum = 0;
+                continue;
+            }
+            $sum += $len;
+            ++$numWordsCurrentLine;
+        }
+
+        if ($numWrappedLines == 0) {
+            $numWordsInWrappedLines = $numWords;
+            $numWrappedLines = 1;
+        } else {
+            $numWordsInWrappedLines = $numWords - $numWordsCurrentLine;
+        }
+
+        if ($numWordsInWrappedLines == 0) {
+            $numWordsInWrappedLines = $numWords;
+            $numWrappedLines = 1;
+        }
+
+        $numWordsInAnchorText = $this->getNumWordsInAnchorText();
+
+        $this->textDensity = $numWordsInWrappedLines / $numWrappedLines;
+        $this->linkDensity = $numWords == 0 ? 0 : $numWordsInAnchorText / $numWords;
+    }
+
+    private function getNumWordsInAnchorText()
+    {
+        $nodes = iterator_to_array($this->container->getElementsByTagName('a'));
+        if (strtolower($this->container->tagName) === 'a') {
+            $nodes[] = $this->container;
+        }
+        $numWords = 0;
+        foreach ($nodes as $node) {
+            $words = mb_split('\s+', $node->textContent);
+            $numWords += count($words);
+        }
+
+        return $numWords;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLinkDensity()
+    {
+        return $this->linkDensity;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTextDensity()
+    {
+        return $this->textDensity;
+    }
+
+    public function getTextContent()
+    {
+        return $this->container->textContent;
+    }
+
+    public function __toString()
+    {
+        $text = preg_replace('#[\r\n]#u', '', $this->container->textContent);
+
+        return sprintf('[ld=%.2f, td=%.2f, short:"%s"]',
+            $this->linkDensity, $this->textDensity, mb_substr($text, 0, 200, 'utf-8'));
+    }
+
+    public function __debugInfo()
+    {
+        return [$this->__toString()];
+    }
+}
---
+++ functions.php
@@ -0,0 +1,222 @@
+<?php
+/**
+ * @author: yevgen
+ * @date: 03.06.17
+ */
+
+function classifyByDensity(ContentBlock $prev, ContentBlock $curr, ContentBlock $next)
+{
+    if ($curr->getLinkDensity() <= 0.333333) {
+        if ($prev->getLinkDensity() <= 0.555556) {
+            if ($curr->getTextDensity() <= 9) {
+                if ($next->getTextDensity() <= 10) {
+                    if ($prev->getTextDensity() <= 4) {
+                        $isContent = false;
+                    } else {
+                        $isContent = true;
+                    }
+                } else {
+                    $isContent = true;
+                }
+            } else {
+                if ($next->getTextDensity() == 0) {
+                    $isContent = false;
+                } else {
+                    $isContent = true;
+                }
+            }
+        } else {
+            if ($next->getTextDensity() <= 11) {
+                $isContent = false;
+            } else {
+                $isContent = true;
+            }
+        }
+    } else {
+        $isContent = false;
+    }
+
+    return $isContent;
+}
+
+
+function iterateThree(array $list)
+{
+    $first = array_slice($list, 0, count($list) - 2);
+    $second = array_slice($list, 1, count($list) - 1);
+    $third = array_slice($list, 2);
+    $it = new MultipleIterator();
+    $it->attachIterator(new ArrayIterator($first));
+    $it->attachIterator(new ArrayIterator($second));
+    $it->attachIterator(new ArrayIterator($third));
+    foreach ($it as $item) {
+        yield $item;
+    }
+}
+
+
+function removeNodes(DOMNode $root, callable $filter)
+{
+    /** @var DOMNode $node */
+    $it = new CallbackFilterIterator(iterateDeepFirst($root, fStubFilter(true)), $filter);
+    foreach ($it as $node) {
+        $node->parentNode->removeChild($node);
+    }
+}
+
+function fNot(callable $filter)
+{
+    return function () use ($filter) {
+        return !call_user_func_array($filter, func_get_args());
+    };
+}
+
+function fStubFilter($return)
+{
+    return function () use ($return) {
+        return $return;
+    };
+}
+
+function fFilterElement() {
+    return function (DOMNode $node) {
+        return $node->nodeType === XML_ELEMENT_NODE;
+    };
+}
+
+function fFilterByTags(array $tags) {
+    return function (DOMNode $node) use ($tags) {
+        return $node instanceof DOMElement && in_array(strtolower($node->tagName), $tags, true);
+    };
+}
+
+function filterByNodeType(array $types)
+{
+    return function (DOMNode $node) use ($types) {
+        return in_array($node->nodeType, $types, true);
+    };
+}
+
+function andFilter(callable... $filters)
+{
+    return function (DOMNode $node) use($filters) {
+        return applyAll($filters, $node);
+    };
+}
+
+function orFilter(callable... $filters)
+{
+    return function (DOMNode $node) use($filters) {
+        return applyAny($filters, $node);
+    };
+}
+
+function applyAll(array $predicates, $object)
+{
+    foreach ($predicates as $fnc) {
+        if (!$fnc($object)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function applyAny(array $predicates, $object)
+{
+    foreach ($predicates as $fnc) {
+        if ($fnc($object)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * @param DOMNode $node
+ * @param callable $filter
+ * @return Generator
+ */
+function iterateDeepFirst(DOMNode $node, callable $filter)
+{
+    foreach (iterate($node->childNodes, $filter) as $child) {
+        yield from iterateDeepFirst($child, $filter);
+        yield $child;
+    }
+}
+
+/**
+ * @param Traversable|null $nodes
+ * @param callable $filter
+ * @return Generator
+ */
+function iterate($nodes, callable $filter)
+{
+    $list = $nodes ? iterator_to_array($nodes) : [];
+    foreach ($list as $node) {
+        if ($filter($node)) {
+            yield $node;
+        }
+    }
+}
+
+/**
+ * @param Iterator $items
+ * @param callable $predicate
+ * @return bool
+ */
+function all(Iterator $items, callable $predicate)
+{
+    foreach ($items as $item) {
+        if (!$predicate($item)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * @return Closure
+ */
+function fIsMarkedAsContent()
+{
+    return function (DOMElement $el) {
+        return isMarkedAsContent($el);
+    };
+}
+
+/**
+ * @param DOMElement $el
+ * @return bool
+ */
+function isMarkedAsContent(DOMElement $el)
+{
+    return $el->hasAttribute('is-content') && (bool) $el->getAttribute('is-content');
+}
+
+/**
+ * @param DOMElement $el
+ */
+function markContent(DOMElement $el)
+{
+    $el->setAttribute('is-content', 1);
+}
+
+function isContent(DOMElement $el)
+{
+    $contentTags = explode(',', 'a,span,i,b,u,p,h1,h2,h3,h4,h5,h6,td,sub,sup,img');
+    if ($el->childNodes->length) {
+        $isContent = all(iterate($el->childNodes, fFilterElement()), fIsMarkedAsContent());
+    } else {
+        $isContent = in_array(strtolower($el->tagName), $contentTags, true);
+    }
+    return $isContent;
+}
+
+function unmarkAsContent(DOMNodeList $nodes)
+{
+    /** @var DOMElement $node */
+    foreach (iterate($nodes, fFilterElement()) as $node) {
+        $node->removeAttribute('is-content');
+    }
+}
